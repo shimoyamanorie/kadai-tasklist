f79d62835650183f861a9962c5b0ed7ccd92abaf
norie.shimoyama <info@nicona.jp>
2019-03-20 09:41:47 +0000

    add columnstatus

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -30,7 +30,13 @@ class TasksController extends Controller
     // postでtasks/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',   // 追加
+            'content' => 'required|max:191',
+        ]);
+        
         $task = new Task;
+        $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
 
@@ -60,7 +66,13 @@ class TasksController extends Controller
     // putまたはpatchでtasks/idにアクセスされた場合の「更新処理」
     public function update(Request$request, $id)
     {
+        $this->validate($request,[
+            'status' => 'required|max:10',   // 追加
+            'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
+        $task->status = $request->status;  // 追加
         $task->content = $request->content;
         $task->save();
 
