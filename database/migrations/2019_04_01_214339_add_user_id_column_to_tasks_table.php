46e2622175dc9cbede38f57867a7d4a900c41532
norie.shimoyama <info@nicona.jp>
2019-04-01 14:00:29 +0000

    migration

--- database/migrations/2019_04_01_214339_add_user_id_column_to_tasks_table.php
+++ database/migrations/2019_04_01_214339_add_user_id_column_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
+class AddUserIdColumnToTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('content');    // content カラム追加
-            $table->timestamps();
+        Schema::table('tasks', function (Blueprint $table){
+            $table->integer('user_id')->unsigned()->index();
         });
-        
     }
 
     /**
